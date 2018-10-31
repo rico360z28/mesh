5376f191a66693310d7b4108c3f58d5b8c8988e8
rico360z28 <ricocantrell@gmail.com>
2018-10-30 17:18:55 -0700

    debugging

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Using Ubuntu
+#curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+#sudo apt-get install -y nodejs
+
+
+#systemctl enable meshcentral.service
+#systemctl start meshcentral.service
+#systemctl status meshcentral.service
