313ecd3cd317823851d9016832e7d682aef26e77
rico360z28 <ricocantrell@gmail.com>
2018-10-30 19:31:15 -0700

    debugging

--- install.sh
+++ install.sh
@@ -1,4 +1,38 @@
 #!/bin/sh
+# Root Check
+exas="$(id -u)"
+if [ "$exas" -gt 0 ] ;
+then
+# we can compare directly with this syntax.
+  echo "Please run as root/sudo"
+  echo "$exas"
+  echo "sudo /.install"
+  exit 1
+else
+  #do your stuff
+  echo "Do Stuff"
+  echo "$exas"
+#
+# Create Mesh Directory
+mkdir ~/meshcentral
+cd ~/meshcentral
+#
+# Installing Nodejs
+#curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+#sudo apt-get install -y nodejs
+#
+#LANIP=
+#WANIP=
+#
+#
+#
+fi
+  echo "Install Completed"
+  echo "Verify and put public https address here"
+  echo "Verify and put private https address here"
+sleep 5
+
+
 # Using Ubuntu
 #curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 #sudo apt-get install -y nodejs
