b0244a0bc5060b38567930cc742d869dc70ae9a2
rico360z28 <32106627+rico360z28@users.noreply.github.com>
2018-11-01 14:08:58 -0700

    Cleaning up

--- install.sh
+++ install.sh
@@ -9,15 +9,11 @@ then
   echo "sudo /.install"
   exit 1
 else
-  #do your stuff
-#
-#
+#do your stuff
 #
 # Create Mesh Directory
 mkdir ~/meshcentral
 cd ~/meshcentral
-#
-#
 #
 # Apt update
 apt update
@@ -43,11 +39,9 @@ echo "Server will be stopped after local cert is created"
 sleep 10
 kill $(ps aux | grep '[m]esh' | awk '{print $2}')
 #
-#
 #Creating Service
 cp ~/mesh/meshcentral.service /etc/systemd/system/meshcentral.service
 cp ~/mesh/meshstart.sh /opt/meshstart.sh
-#
 #
 #Installing Service
 systemctl enable meshcentral.service
@@ -60,13 +54,8 @@ lanip="$(hostname -I | tr -d ' ')"
 wanip="$(curl ifconfig.me )"
 port="$(netstat -pant | grep -m1 '[n]ode' | awk '{print $4}' | cut -f2 -d":" | tail -n1)"
 #
-#
-#
 fi
   echo "Install Completed for $machine "
   echo "Meshcentral 2 is available at (if open to internet) $wanip:$port "
   echo "Meshcentral 2 is available at $lanip:$port "
 sleep 5
-
-
-
